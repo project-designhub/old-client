fa7fa3c648aee49f0d77ce21276f62f13ab31eb1
Javontay McElroy <dinomightnetwork@gmail.com>
2019-07-08 15:10:34 -0500

    added react-router-dom, axios, sass.

---
+++ designhub/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Project: DesignHub :P</h1>
+        <p>
+          Designhub is a codename cause it's already taken... so don't sue us!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
