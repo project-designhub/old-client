665028cca76bd8a7bf615710487cc8a22dd760ad
Javontay McElroy <dinomightnetwork@gmail.com>
2019-07-08 20:01:50 -0500

    Created Profile component.

--- designhub/src/App.js
+++ designhub/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import { Switch, Route } from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
-import Dashboard from './components/Dashboard';
-import Onboard from './components/Onboard';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+import { Switch, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./components/Dashboard";
+import Onboard from "./components/Onboard";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <NavBar />
-        <h1>Welcome to Project: DesignHub :P</h1>
-        <p>
-          Designhub is a codename cause it's already taken... so don't sue us!
-        </p>
-      </header>
+      <NavBar />
       <Switch>
         <Route exact path="/" />
         <PrivateRoute path="/dashboard" component={Dashboard} />
         <PrivateRoute path="/onboard" component={Onboard} />
+        <Route path="/profile" component={UserProfile} />
       </Switch>
     </div>
   );
---
+++ designhub/src/components/UserProfile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Component } from "react";
+
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello from User Profile</h1>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
