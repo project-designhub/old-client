5b6e5413a2da98c3190f4b281aaf00fdbedb562b
cecil john tantay <cjbtantay@gmail.com>
2019-08-07 21:57:50 -0700

    add loading component

--- designhub/src/App.js
+++ designhub/src/App.js
@@ -4,22 +4,30 @@ import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './auth/PrivateRoute';
 import Dashboard from './components/Dashboard';
 import Onboard from './components/Onboard';
-
+import { useAuth0 } from './auth/authWrapper';
 import CreateProject from './components/CreateProject';
 import UserProfile from './components/UserProfile';
+import Loading from './components/Loading';
 
 function App() {
+  const { loading } = useAuth0();
   return (
-    <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/" />
-        <PrivateRoute path="/dashboard" component={Dashboard} />
-        <PrivateRoute path="/onboard" component={Onboard} />
-        <Route path="/createproject" component={CreateProject} />
-        <Route path="/profile" component={UserProfile} />
-      </Switch>
-    </div>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
+            <PrivateRoute path="/onboard" component={Onboard} />
+            <Route path="/createproject" component={CreateProject} />
+            <Route path="/profile" component={UserProfile} />
+          </Switch>
+        </>
+      )}
+    </>
   );
 }
 
---
+++ designhub/src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div
+      style={{ display: 'flex', justifyContent: 'center', fontSize: '5rem' }}
+    >
+      ...Loading
+    </div>
+  );
+};
+
+export default Loading;
